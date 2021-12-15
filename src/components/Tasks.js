743c678cd07b0f831912d8e19aa5259e3ccca251
JayJaber <calijayjaber@gmail.com>
2021-12-14 18:21:59 -0800

    fixed spacing

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -17,14 +17,18 @@ export default function Tasks ({tasks, onTasksChange, color}) {
       
       {/* List of tasks */}
       <TextInput value={title} label='Edit Title' placeholder='Add Title' onChange={e => setTitle(e.target.value)} style={{width: 'calc(100% - 0.9em)', margin: 0, textAlign: 'center', fontSize: 24}}></TextInput>
+      {/* <div class="input-field col">
+      <input value="Alvin" id="first_name2" type="text" class="validate" style={{margin: 0, padding: 0}}/>
+      <label class="active" for="first_name2">First Name</label>
+       </div> */}
 
       {tasks.length === 0? <h6 style={{padding: '1.27em'}}>No tasks yet, add some below!</h6>
       : <Row style={{marginBottom: '0', marginTop: 0}}>
           {/* <Col s={12} m={12}><div  style={{borderBottom: '1px solid rgb(158, 158, 158)', paddingTop: '0.1em', paddingBottom: '0.35em'}}></div></Col> */}
         {tasks.map((task, i) => 
           <>
-            <Col s={9} m={10} l={10} xl={11} style={{textAlign: 'left', marginTop: '1em', marginBottom: '0.5em'}}>{task.done? <s>{i+1}.&nbsp;{task.value}</s> : <>{i+1}.&nbsp;{task.value}</> }</Col>
-            <Col s={3} m={2} l={2} xl={1} style={{marginTop: '1em', marginBottom: '0.5em', textAlign: 'right'}}><Checkbox label={task.done? 'Done': 'Todo'} checked={task.done} value='done'
+            <Col s={9} m={10} l={10} xl={11} style={{textAlign: 'left', marginTop: (i===0? '0.25em':'1em'), marginBottom: '0.5em'}}>{task.done? <s>{i+1}.&nbsp;{task.value}</s> : <>{i+1} {task.value}</> }</Col>
+            <Col s={3} m={2} l={2} xl={1} style={{marginTop: (i===0? '0.25em':'1em'), marginBottom: '0.5em', textAlign: 'right'}}><Checkbox label={task.done? 'Done': 'Todo'} checked={task.done} value='done'
               onClick={e => onTasksChange( tasks.map((task, j) => { if(i ===j)return {...task, done: e.target.checked}; return task;}))}></Checkbox></Col>
             <Col s={12} m={12}><div  style={{borderBottom: '1px solid rgb(158, 158, 158)', paddingTop: '0.05em', paddingBottom: '0.2em'}}></div></Col>
           </>
